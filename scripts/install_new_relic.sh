3bd7e2d8e617012f904b2e8f93be7640cf20c0f0
brandondeyoung <brandon_deyoung@harvard.edu>
2015-08-12 13:02:17 -0400

    removed quotes

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY="67fc1d48c2debab6f2d1dfcc32b675fcec0a0442"
+export NR_INSTALL_KEY=67fc1d48c2debab6f2d1dfcc32b675fcec0a0442
 newrelic-install install
